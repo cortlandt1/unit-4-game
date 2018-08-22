367fd2fe3e471cddc2fcd559b7be2217e3fef4db
Gordo <cortland.taylor1@gmail.com>
2018-08-21 17:56:42 -0700

    Rough jQuery written

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,9 +14,107 @@ let targetScore = 0
 
 
 //function to assign number to crystals
- $('.crystal').append(`
- data-crystal=${Math.floor(Math.random() * 25) + 1 }
- `)
+//  $('#crystal1').append(`
+//  data-crystal=${Math.floor(Math.random() * 25) }
+//  `)
+// function displayScore () {
+//     $('.goal').append(`Your target score to match is ${targetScore}`)
+// }
+
+
+//Creates the target score and appends it to the index.html
+targetScore = Math.floor(Math.random() * 150) + 1
+$('.goal').text(targetScore)
+
+//Generates the random numbers for the crystals
+let one = Math.floor(Math.random() * 25) + 1
+let two = Math.floor(Math.random() * 25) + 1
+let three = Math.floor(Math.random() * 25) + 1
+let four = Math.floor(Math.random() * 25) + 1
+$('.scoreTotal').text(currentScore);
+
+//Functions for winning 
+function win () {
+    wins++
+    alert('You Win!')
+    $('.wins').text(wins)
+    reset()
+}
+//Function for losing
+function lose () {
+    losses++
+    alert('You lose!')
+    $('.losses').text(losses)
+    reset()
+}
+
+//Functions for click events and deciding whether you've won or
+$('#crystal1').on('click', function (){
+    currentScore = one + currentScore
+    $('.scoreTotal').text(currentScore);
+    if (currentScore == targetScore) {
+        win()
+    }
+    else if (currentScore > targetScore) { 
+        lose()
+    }
+})
+
+$('#crystal2').on('click', function (){
+    currentScore = two + currentScore
+    $('.scoreTotal').text(currentScore);
+    if (currentScore == targetScore) {
+        win()
+    }
+    else if (currentScore > targetScore) { 
+        lose()
+    }
+})
+
+$('#crystal3').on('click', function (){
+    currentScore = three + currentScore
+    $('.scoreTotal').text(currentScore);
+    if (currentScore == targetScore) {
+        win()
+    }
+    else if (currentScore > targetScore) {
+        lose()
+    }
+})
+
+$('#crystal4').on('click', function (){
+    currentScore = four + currentScore
+    $('.scoreTotal').text(currentScore);
+    if (currentScore == targetScore) {
+        win()
+    }
+    else if (currentScore > targetScore) { 
+        lose()
+    }
+})
+
+
+function reset () {
+    currentScore = 0
+    one = Math.floor(Math.random() * 25) + 1
+    two = Math.floor(Math.random() * 25) + 1
+    three = Math.floor(Math.random() * 25) + 1
+    four = Math.floor(Math.random() * 25) + 1
+    targetScore = Math.floor(Math.random() * 150) + 1
+    $('.scoreTotal').text(currentScore);
+    $('.goal').text(targetScore)
+    // $('.goal').append(`Your target score to match is ${targetScore}`)
+}
+// $('#crystal1').on('click', function (){
+//     let crystalValue = $(this).attr('data-crystal')
+//     crystalValue = parseInt(crystalValue)
+//     crystalValue += currentScore
+//     $('.scoreTotal').text(currentScore)
+//     console.log(crystalValue)
+// })
+
+
+
 
 //  $('.crystal').data (function() {
     
@@ -26,10 +124,4 @@ let targetScore = 0
 
 
 
-$('.crystal').on('click', function (){
-    let crystalValue = $('.crystal').attr('data-crystal')
-    currentScore += parseInt(crystalValue)
-    $('.scoreTotal').text(currentScore)
-})
 
-console.log(currentScore)
